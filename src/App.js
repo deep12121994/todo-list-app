a06eab6edd31e5221d78766eb304fbffcbda91b6
deep12121994 <balasdeep@gmail.com>
2021-12-18 20:12:47 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
 import DetailsPage from './components/DetailsPage';
 import './App.css';
@@ -9,10 +10,15 @@ class App extends React.Component {
     return (<>
       <div className="header-container">
         <header>Warehouse Information</header>
+        <button id="btn-item">Item</button>
       </div>
-      <div className="app">
-        <SearchPage/>
-        {/* <DetailsPage/> */}
+      <div className="app">      
+        <BrowserRouter>
+          <Routes>
+          <Route path="/" component={SearchPage}/>
+          <Route path="/details" component={DetailsPage}/>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>    
     );
